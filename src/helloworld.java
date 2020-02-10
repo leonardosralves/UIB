fdd113f636c6b9a5a5afb48d4d70bbccd3910d5d
Leonardo Souto Rodrigues Alves <1192307310@acd.grupotiradentes.com>
2020-02-10 19:03:06 -0300

    Digite seu nome.

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 
 public class helloworld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World!");
-
+		Scanner leTeclado = new Scanner (System.in);
+		System.out.println("Digite seu nome!");
+		String nome = leTeclado.next();
+		System.out.println ("Seu nome é" + nome);
 	}
 
 }
